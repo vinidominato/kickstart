c07807397db1238fc0f73309224b4a36989748e8
Vinicio Dominato <141687902+vinidominato@users.noreply.github.com>
2023-08-15 15:57:08 +0000

    adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-console.log(Enviando para GitHub)
+console.log("Enviando para GitHub")
